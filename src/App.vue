7f347ec31aad28fb2e5bcb3baf08dc93c4e7b2f4
unknown <tevin.omondi@strathmore.edu>
2024-05-19 22:11:30 +0300

    Added some features in the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/feed">Feed</router-link> |
-    <router-link to="/register">Register</router-link> |
-    <router-link to="/signin">SignIn</router-link>
-    <button class="signOut" v-if="isLoggedIn" @click="handleSignOut">Sign Out</button>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
-// Want the signout to be available when user is signed In
-import { onMounted, ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+<script>
 
-const isLoggedIn = ref(false);
-const router = useRouter();
-const auth = getAuth();
+export default {
+  name: 'App',
 
-// Use a hook called onAuthStateChanged
-onMounted(() => {
-  onAuthStateChanged(auth, (user) => {
-    isLoggedIn.value = !!user;
-  });
-});
-
-const handleSignOut = () => {
-  signOut(auth).then(() => {
-    router.push('/');
-  });
-};
+  data: () => ({
+    //
+  }),
+}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app-bar app color="black" dark flat class="px-12" >
+      <v-btn >
+        <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon> AHLAN
+      </v-btn>
+      
+  
+      <v-spacer></v-spacer>
+      <v-btn text @click="scroll('home')" class="text-yellow">Home</v-btn>
+      <v-btn text @click="scroll('about')"  >About</v-btn>
+      <v-btn text @click="scroll('mission')"  >Mission</v-btn> 
+     <v-btn text @click="scroll('services')" >Services</v-btn>
+      <v-btn text @click="scroll('testimonials')"  >Testimonials</v-btn>   
+      <v-btn text @click="scroll('howtohelp')"  >How to help</v-btn>
+      <v-btn text @click="scroll('contact')"  >Contact Us</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ behavior: "smooth" });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
