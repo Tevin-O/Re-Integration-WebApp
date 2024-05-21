1a294b6993ab7d63c40c0ed216bfbde1c5feef26
unknown <tevin.omondi@strathmore.edu>
2024-05-21 13:57:53 +0300

    Added the logout functionality to the feed page and a welcoming message

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ export default VuetifyComponents
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+//import { getAuth } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -41,8 +42,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
-// const auth = getAuth(app);
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
 
 loadFonts()
 
